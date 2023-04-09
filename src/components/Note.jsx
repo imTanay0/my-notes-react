c606bd1a47fe1726762759680e1b975ab6b750dd
imtanay <107740662+imTanay0@users.noreply.github.com>
2023-04-09 18:17:52 +0530

    bug fixes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,69 +1,84 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ShowNotes from './ShowNotes';
 
 const Note = () => {
 
-  const [notes, setNotes] = useState([]);
+  const [newTitle, setNewTitle] = useState('');
+  const [newContent, setNewContent] = useState('');
+  const [notes, setNotes] = useState(() => {
 
-  function renderNotes() {
-    return (
-      <div className='grid justify-items-center justify-center xl:grid-cols-3 lg:grid-cols-2 mx-[50px] mt-[50px] md:grid-cols-2 sm:grid-cols-1 sm:gap-6' >
-        {notes.map((note, idx) => (
-          <ShowNotes key={idx} index={idx} title={note.title} note={note.note} handleDeletNote={handleDeletNote} />
-        ))}
-      </div>
-    );
-  }
+    const localNoteValue = localStorage.getItem('NOTES');
 
+    if (localNoteValue == null) return [];
+
+    return JSON.parse(localNoteValue);
+
+  });
+
+  useEffect(() => {
+    localStorage.setItem("NOTES", JSON.stringify(notes));
+  }, [notes])
 
   function HandleSubmitNote(e) {
     e.preventDefault();
 
-    const titleText = document.getElementById('titleInput').value;
-    const noteText = document.getElementById('noteInput').value;
+    let titleText = document.getElementById('titleInput').value;
+    let noteText = document.getElementById('noteInput').value;
 
     if (titleText === '' || noteText === '') {
       alert('Enter Title & Content of the Note.')
       return;
     } else {
-      setNotes([
-        ...notes,
-        {
-          title: titleText,
-          note: noteText,
-        }
-      ]);
+
+      setNotes(currentNotes => {
+        return [
+          ...currentNotes,
+          {
+            id: crypto.randomUUID(),
+            title: newTitle,
+            note: newContent
+          }
+        ]
+      })
+
+      setNewTitle('')
+      setNewContent('')
     }
   }
 
-  const handleDeletNote = (index) => {
-    // console.log(`Note with index ${index} is deleted`);
+  const handleDeletNote = (id) => {
+    setNotes(currentNotes => {
+      return currentNotes.filter(note => note.id !== id)
+    });
+  }
 
-    const div = document.getElementById(`${index}`)
-
-    // console.log(div);
-
-    div.style.display = 'none';
-
-    let deletedNotes = notes.splice(index, 1);
-    const newNotes = notes;
-    console.log('Nw Note: ', newNotes);
-
-    setNotes(newNotes);
+  function renderNotes() {
+    return (
+      <>
+        {notes.length === 0 && <p className='px-10 text-xl text-center mt-20'>You currently have no notes...</p>}
+        <div className='grid justify-items-center justify-center xl:grid-cols-3 lg:grid-cols-2 mx-[50px] mt-[50px] md:grid-cols-2 sm:grid-cols-1 sm:gap-6' >
+          {notes.map(note => (
+            <div key={note.id}>
+              <ShowNotes id={note.id} title={note.title} note={note.note} handleDeletNote={() => handleDeletNote(note.id)} />
+            </div>
+          ))}
+        </div>
+      </>
+    );
   }
 
   return (
     <>
       <div className='flex items-center justify-center my-10'>
-        <form className='border p-8 rounded-md'>
+        <form onSubmit={e => HandleSubmitNote(e)} className='border p-8 rounded-md'>
           <div className='flex flex-col w-full'>
-            <label>Title: </label>
-            <input id='titleInput' className='w-[300px] rounded-sm text-slate-900 px-2' type="text" />
+            <label htmlFor='titleInput'>Title:</label>
+            <input id='titleInput' value={newTitle} className='mt-1.5 w-[300px] rounded-sm text-slate-900 px-2' type="text" onChange={e => setNewTitle(e.target.value)} />
 
-            <label className='mt-3'>Note: </label>
-            <textarea id='noteInput' rows="4" cols="39" className='w-[300px] rounded-sm text-slate-900 px-2'></textarea>
+            <label htmlFor='noteInput' className='mt-3'>Content:</label>
+            <textarea id='noteInput' value={newContent} rows="4" cols="39" className='mt-1.5 w-[300px] rounded-sm text-slate-900 px-2' onChange={e => setNewContent(e.target.value)}></textarea>
           </div>
-          <button className='mt-5 relative left-[118px] border px-2 py-1 rounded-md hover:bg-slate-200 hover:text-black' onClick={(e) => HandleSubmitNote(e)}>Add Note</button>
+          <button className='mt-5 relative left-[118px] border px-2 py-1 rounded-md hover:bg-slate-200 hover:text-black'>Add Note</button>
         </form>
       </div>
       {renderNotes()}
--- src/components/ShowNotes.jsx
+++ src/components/ShowNotes.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const ShowNotes = ({ index, title, note, handleDeletNote }) => {
+const ShowNotes = ({ key, id, title, note, handleDeletNote }) => {
 
   return (
     <>
-      <div id={`${index}`} className='flex border flex-col items-center p-5 rounded-md mb-10 lg:w-[350px] md:w-[300px] w-[280px]'>
+      <div className='flex border flex-col items-center p-5 rounded-md mb-10 lg:w-[350px] md:w-[300px] w-[280px]'>
         <p className='text-xl mb-4'>{title}</p>
         <p>{note}</p>
         <div className='relative mt-10 mb-3 w-full h-[2px] bg-white'></div>
-        <button className='border rounded-sm px-3 py-1 mt-3' onClick={() => handleDeletNote(index)}>Delete Note</button>
+        <button className='border rounded-sm px-3 py-1 mt-3' onClick={() => handleDeletNote(id)}>Delete Note</button>
       </div>
     </>
   )
